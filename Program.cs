db2ce5925a91268b4a06a65f533d5f5d01cab669
днс <@LT-KKG25BBH>
2022-10-14 01:24:35 +1100

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Temp";
+            if (Directory.Exists(path))
+            {
+                string[] fList= Directory.GetFiles(path,"*.*",SearchOption.AllDirectories);
+            
+                foreach (string f in fList)
+                {
+                    Console.WriteLine(f);
+                }
+            }
+            else
+            { Console.WriteLine("Папки не существует");
+            }
+        Console.ReadKey();
+        
+        }
+    }
+}
